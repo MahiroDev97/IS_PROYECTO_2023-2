6762e95ceb6a0a52bfcd4c535a0a697e31abf446
Mario Vidal <mariovidal16@gmail.com>
2023-12-18 17:14:21 -0300

    Login Changes

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { login } from '../services/auth.service';
-import "/styles/LoginForm.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import "../login.scss";
+
 function LoginForm() {
   const navigate = useNavigate();
 
@@ -18,20 +21,32 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        name="email"
-        type="email"
-        {...register('email', { required: true })}
-      />
-      <input
-        type="password"
-        name="password"
-        {...register('password', { required: true })}
-      />
-      {errors.exampleRequired && <span>This field is required</span>}
-      <input type="submit" />
-    </form>
+    <Box component="form" noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)} className="App">
+      <div className="container">
+        <div className="card_area">
+          <h1 className="container_title">Back to your digital life</h1>
+          <p className="container_para">Choose one of the option to go</p>
+          <div className="input_card">
+            <input
+              name="email"
+              type="email"
+              {...register('email', { required: true })}
+              className="input"
+            />
+            <input
+              type="password"
+              name="password"
+              {...register('password', { required: true })}
+              className="input"
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
+          </div>
+          <div className="card_button">
+            <input type="submit" className="button-28" />
+          </div>
+        </div>
+      </div>
+    </Box>
   );
 }
 
