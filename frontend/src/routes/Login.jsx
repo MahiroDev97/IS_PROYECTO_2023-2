6095aef340c941d29fb1e0dad490783549b8a980
Mario Vidal <mariovidal16@gmail.com>
2023-12-18 16:08:58 -0300

    Cambios front

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,5 +1,8 @@
 import LoginForm from '../components/LoginForm';
 import { useNavigate } from 'react-router-dom';
+import '../login.scss';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function Login() {
   const navigate = useNavigate();
@@ -14,10 +17,33 @@ function Login() {
   }
 
   return (
-    <div>
-      <h2>Inicia sesion!</h2>
-      <LoginForm />
-    </div>
+    <Box component="form" noValidate autoComplete="off">
+      <div className="App">
+        <div className="container">
+          <div className="card_area">
+            <h1 className="container_title">Postulaciones Patentes Comerciales</h1>
+            <p className="container_para">Choose one of the option to go</p>
+            <div className="input_card">
+              <TextField
+                className="input"
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+              />
+              <TextField
+                className="input"
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+              />
+            </div>
+            <div className="card_button">
+              <button class="button-28">Log in</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Box>
   );
 }
 
