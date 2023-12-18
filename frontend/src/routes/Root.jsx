be3606a8da449a1097d86bc581ee25c8f2ec20ac
Mario Vidal <mariovidal16@gmail.com>
2023-12-18 18:03:28 -0300

    cambios front

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -2,6 +2,8 @@ import { Outlet } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { logout } from '../services/auth.service';
 import { AuthProvider, useAuth } from '../context/AuthContext';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import "/styles/root.css";
 
 function Root() {
   return (
@@ -15,8 +17,10 @@ function PageRoot() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    logout();
-    navigate('/auth');
+    if (window.confirm("¿Estás seguro de que quieres cerrar la sesión?")) {
+      logout();
+      navigate('/auth');
+    }
   };
 
   const { user } = useAuth();
@@ -24,7 +28,14 @@ function PageRoot() {
   return (
     <div>
       <div>
-        <h1>Aqui deberia ir un header</h1>
+      <Sidebar className='sidebar'>
+        <Menu className='menu'>
+          <MenuItem className='menu-item'> Información </MenuItem>
+          <MenuItem> Postular </MenuItem>
+          <MenuItem> Mis Postulaciones </MenuItem>
+          <MenuItem onClick={handleLogout}> Cerrar Sesion </MenuItem>
+        </Menu>
+      </Sidebar>
         <p>Estas logeado como: {user.email}</p>
         <button onClick={handleLogout}>Cerrar sesion</button>
       </div>
