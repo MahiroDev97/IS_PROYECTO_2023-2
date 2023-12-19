038843da0791ec43ff2df56f7e784a9b36fd9413
zmaru <cristobal.munoz1601@alumnos.ubiobio.cl>
2023-12-18 21:12:51 -0300

    Cambios menores

---
+++ frontend/src/routes/Postulaciones.jsx
@@ -0,0 +1,29 @@
+import { AuthProvider, useAuth } from '../context/AuthContext';
+import { Layout} from 'antd';
+import MenuList from '../components/MenuList';
+import "/styles/Root.css";
+
+const {Content,Sider } = Layout;
+
+function Root() {
+  return (
+    <AuthProvider>
+      <PagePostulaciones />
+    </AuthProvider>
+  );
+}
+
+function PagePostulaciones() {
+  const { user } = useAuth();
+  return (
+    <Layout>
+      <MenuList/>
+      <Content className="dark-background">
+        <h1 className='title'>Mis Postulaciones</h1>
+        
+      </Content>
+    </Layout>
+  );
+}
+
+export default Postulaciones;
