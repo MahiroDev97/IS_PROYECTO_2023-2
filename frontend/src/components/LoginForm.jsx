480da908aba0d50d9dbb11c6edecf64cdb68bc06
zmaru <cristobal.munoz1601@alumnos.ubiobio.cl>
2024-01-01 04:46:34 -0300

    Css Login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { login } from '../services/auth.service';
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { login } from "../services/auth.service";
+import "../styles/LoginStyles.css";
 
 function LoginForm() {
   const navigate = useNavigate();
@@ -13,30 +14,67 @@ function LoginForm() {
 
   const onSubmit = (data) => {
     login(data).then((roles) => {
-      const isAdmin = roles.some(role => role.name === 'admin');
+      const isAdmin = roles.some((role) => role.name === "admin");
       if (isAdmin) {
-        navigate('/admin');
+        navigate("/admin");
         return;
       }
-      navigate('/');
+      navigate("/");
     });
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        name="email"
-        type="email"
-        {...register('email', { required: true })}
-      />
-      <input
-        type="password"
-        name="password"
-        {...register('password', { required: true })}
-      />
-      {errors.exampleRequired && <span>This field is required</span>}
-      <input type="submit" />
-    </form>
+    // <form onSubmit={handleSubmit(onSubmit)}>
+    //   <input
+    //     name="email"
+    //     type="email"
+    //     {...register('email', { required: true })}
+    //   />
+    //   <input
+    //     type="password"
+    //     name="password"
+    //     {...register('password', { required: true })}
+    //   />
+    //   {errors.exampleRequired && <span>This field is required</span>}
+    //   <input type="submit" />
+    // </form>
+
+    <div className="loginBox">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Iniciar Sesion</h2>
+
+        <label htmlFor="uname">
+          {" "}
+          <b>Email</b>{" "}
+        </label>
+        <br />
+        <input
+          type="email"
+          name="email"
+          placeholder="correo@mail.com"
+          {...register("email", { required: true })}
+        />
+
+        <br />
+
+        <label htmlFor="psw">
+          {" "}
+          <b>Contraseña</b>{" "}
+        </label>
+        <br />
+        <input
+          name="password"
+          type="password"
+          placeholder="contraseña"
+          {...register("password", { required: true })}
+        />
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <br />
+
+        <button type="submit">Ingresar</button>
+      </form>
+    </div>
   );
 }
 
--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,21 +1,21 @@
-import LoginForm from '../components/LoginForm';
-import { useNavigate } from 'react-router-dom';
+import LoginForm from "../components/LoginForm";
+import { useNavigate } from "react-router-dom";
+import "../styles/LoginStyles.css";
 
 function Login() {
   const navigate = useNavigate();
 
-  if (localStorage.getItem('user')) {
+  if (localStorage.getItem("user")) {
     return (
       <>
         <h2>Ya estas logeado!</h2>
-        <button onClick={() => navigate('/')}>Ir a home</button>
+        <button onClick={() => navigate("/")}>Ir a home</button>
       </>
     );
   }
 
   return (
-    <div>
-      <h2>Inicia sesion!</h2>
+    <div className="loginPage">
       <LoginForm />
     </div>
   );
